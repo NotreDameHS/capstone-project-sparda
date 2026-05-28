f8a9e8230e7b7254d3805ae64086e943b58ce71e
K1Kam <kamsiugbala@gmail.com>
2026-05-28 13:58:04 -0600

    mob
    continue

---
+++ base_mob.gd
@@ -0,0 +1,19 @@
+extends Area2D
+@export var health = 100.0
+@export var max_health = 100.0
+@onready	 var HP = $Control/HP  
+
+func damage(amount: float) -> void:
+		health -= amount
+		if health <= 0:
+			queue_free() 
+		HP.value = health
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
